d13ef7f4287bcfaaa0862061be0f7d70c4cfaf88
kashyapSheladiya <kashyap27041993@gmail.com>
2019-05-06 11:53:24 +0200

    Using let method to avoid duplication of code.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   context 'validation tests' do
+    
     it 'first name should be always present' do
       user = User.new(last_name: 'last', email: 'xxx@exe.com').save
       expect(user).to eq(false)
@@ -17,13 +18,15 @@ RSpec.describe User, type: :model do
       expect(user).to eq(false)
     end
 
+    let(:emailParams) { {first_name: 'first', last_name: 'last'} }
+
     it 'email should be valid' do
-      user1 = User.new(first_name: 'first', last_name: 'last', email: 'xxxexe.com').save
-      user2 = User.new(first_name: 'first', last_name: 'last', email: 'xxx@execom').save
-      user3 = User.new(first_name: 'first', last_name: 'last', email: 'xxx@exe.67').save
-      user4 = User.new(first_name: 'first', last_name: 'last', email: 'xxxexecom').save
-      user5 = User.new(first_name: 'first', last_name: 'last', email: '@exe.com').save
-      user6 = User.new(first_name: 'first', last_name: 'last', email: 'xxx@.com').save
+      user1 = User.new(emailParams.merge(email: 'xxxexe.com')).save
+      user2 = User.new(emailParams.merge(email: 'xxx@execom')).save
+      user3 = User.new(emailParams.merge(email: 'xxx@exe.67')).save
+      user4 = User.new(emailParams.merge(email: 'xxxexecom')).save
+      user5 = User.new(emailParams.merge(email: '@exe.com')).save
+      user6 = User.new(emailParams.merge(email: 'xxx@.com')).save
       expect(user1).to eq(false)
       expect(user2).to eq(false)
       expect(user3).to eq(false)
@@ -39,12 +42,13 @@ RSpec.describe User, type: :model do
   end
 
   context 'scope tests' do
+    let(:params) { {first_name: 'first', last_name: 'last', email: 'xxx@exe.com'} }
     before(:each) do
-      User.new(first_name: 'first', last_name: 'last', email: 'xxx@exe.com').save
-      User.new(first_name: 'first', last_name: 'last', email: 'xxx@exe.com').save
-      User.new(first_name: 'first', last_name: 'last', email: 'xxx@exe.com', active: false).save
-      User.new(first_name: 'first', last_name: 'last', email: 'xxx@exe.com', active: false).save
-      User.new(first_name: 'first', last_name: 'last', email: 'xxx@exe.com', active: true).save
+      User.new(params).save
+      User.new(params).save
+      User.new(params.merge(active: false)).save
+      User.new(params.merge(active: false)).save
+      User.new(params.merge(active: true)).save
     end
 
     it 'should return active users' do
